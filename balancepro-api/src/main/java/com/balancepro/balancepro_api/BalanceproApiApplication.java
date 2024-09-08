0711bc9cd3596dd76985589a23328e15b1f47a3a
molalenim <molaleni.mabaso@gmail.com>
2024-09-08 13:01:57 -0400

    added start.spring.io back end template

---
+++ balancepro-api/src/main/java/com/balancepro/balancepro_api/BalanceproApiApplication.java
@@ -0,0 +1,13 @@
+package com.balancepro.balancepro_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceproApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceproApiApplication.class, args);
+	}
+
+}
---
+++ balancepro-api/src/test/java/com/balancepro/balancepro_api/BalanceproApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balancepro.balancepro_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceproApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
